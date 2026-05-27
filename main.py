2c72bf430477d6c4856684f02b55763236afaaa5
clencyc <clency2023@gmail.com>
2026-05-27 21:43:45 +0300

    main.py

--- main.py
+++ main.py
@@ -26,7 +26,11 @@ app.add_middleware(
 
 app.include_router(router, prefix="/api")
 
+
 @app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def root():
